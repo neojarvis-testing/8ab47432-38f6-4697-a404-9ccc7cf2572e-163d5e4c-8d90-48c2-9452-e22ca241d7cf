ed9727dc71ce7b5c49875aaf9eebbed99fba2fd7
Poojitha-2015 <poojitha.sudini@gmail.com>
2025-04-21 13:25:41 +0000

    message

--- Project/src/test/java/pages/SwarovskiHomePage.java
+++ Project/src/test/java/pages/SwarovskiHomePage.java
@@ -1,5 +1,63 @@
 package pages;
-
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import uistore.SwarovskiHomePageLocators;
+import utils.AssertionUtility;
+import utils.Base;
+import utils.LoggerHandler;
+import utils.WebDriverHelper;
 public class SwarovskiHomePage {
+    public static ExtentTest test;
+    public static WebDriverHelper helper;
+    public SwarovskiHomePage(ExtentTest test){
+        this.test=test;
+        helper=new WebDriverHelper(Base.driver);
+    }
+    public void hoverOnAccessories(){
+        try{
+            helper.waitForElementToBeVisible(SwarovskiHomePageLocators.hoverOnAccessories, 10);
+            helper.hoverElement(SwarovskiHomePageLocators.hoverOnAccessories);
+            LoggerHandler.logInfo("hover on Accessories");
+            test.log(Status.PASS, "hover on Accessories");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            LoggerHandler.logError(" not hover on Accessories");
+            test.log(Status.FAIL,"not hover on Accessories");
+        }
+        
+    }
+    public void clickOnSunglasses(){
+        try{
+            helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnSunglasses, 10);
+            helper.hoverElement(SwarovskiHomePageLocators.clickOnSunglasses);
+            LoggerHandler.logInfo("clickOnSunglasses");
+            test.log(Status.PASS, "clickOnSunglasses");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            LoggerHandler.logError(" not clickOnSunglasses");
+            test.log(Status.FAIL,"not clickOnSunglasses");
+        }
+    }
+
+    public void verifyUrlSunglasses(){
+        String url=Base.driver.getCurrentUrl();
+        AssertionUtility.verifyCondition(test, url,"Sunglasses");
+
+    }
+    public void clickOnMaterial(){
+        try{
+            helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnSunglasses, 10);
+            helper.hoverElement(SwarovskiHomePageLocators.clickOnSunglasses);
+            LoggerHandler.logInfo("clickOnSunglasses");
+            test.log(Status.PASS, "clickOnSunglasses");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            LoggerHandler.logError(" not clickOnSunglasses");
+            test.log(Status.FAIL,"not clickOnSunglasses");
+        }
+    }
 
 }
--- Project/src/test/java/uistore/SwarovskiHomePageLocators.java
+++ Project/src/test/java/uistore/SwarovskiHomePageLocators.java
@@ -1,5 +1,24 @@
 package uistore;
 
+import org.openqa.selenium.By;
+
 public class SwarovskiHomePageLocators {
+    public static By hoverOnAccessories  = By.cssSelector("#navAccessoriesLink");
+    public static By clickOnSunglasses=By.cssSelector("#navAccessoriesSunglassesLink");
+    public static By clickOnMaterial=By.cssSelector("span[id='filter-dropdown-count-product_material']") ;
+    public static By clickOnAcetate=By.cssSelector("#dropdown-facet-value-list-product_material > li:nth-child(0)");
+    public static  By clickOnShowProducts=By.cssSelector("span.swa-button__text swa-label-sans--medium-strong");
+    public static By clickOnFirstProduct=By.xpath("//span[text()='Pillow shape, Purple']");
+    public static By clickonCareandMaintainence=By.cssSelector("#tabby-toggle_swa-accessible-ALWAYSON_PDP_ACC-ENTRY_CARE_ROW");
+    public static By bookAnAppointmnet=By.cssSelector("#tabby-toggle_swa-accessible-ALWAYSON_PDP_ACC-ENTRY_Book-an-appointment");
+    public static By verifyLogin=By.xpath("(//a[@class='swa-link js-swa-header-login js-header-links'])[1]");
+
+
+
+    
+
+
+
+
 
 }
