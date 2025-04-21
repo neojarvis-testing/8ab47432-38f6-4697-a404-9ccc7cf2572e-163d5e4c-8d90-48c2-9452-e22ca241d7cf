6445bd246c5f941e4aab96deec353eb682b15c02
Sarika200 <sarikachennarapu@gmail.com>
2025-04-21 13:30:16 +0000

    cleartree

--- Project/src/test/java/uistore/SwarovskiHomePageLocators.java
+++ Project/src/test/java/uistore/SwarovskiHomePageLocators.java
@@ -1,5 +1,19 @@
 package uistore;
 
-public class SwarovskiHomePageLocators {
+import org.openqa.selenium.By;
 
+public class SwarovskiHomePageLocators {
+    public static By clickOnAboutSwarovski=By.xpath("(//span[text()='About Swarovski'])[8]");
+    public static By clickOnSustainability=By.xpath("(//span[text()='Sustainability'])[8]");
+    public static By clickOnJobsAndCareer=By.xpath("(//span[text()='Jobs & Career'])[8]");
+    public static By clickOnAlumniCommunity=By.xpath("(//span[text()='Alumni Community'])[1]");
+    public static By clickOnCorporategifts=By.xpath("(//span[text()='Corporate Gifts'])[8]");
+    public static By clickOnForProfessional=By.xpath("(//span[text()='For Professionals'])[8]");
+    public static By clickOnSitemap=By.xpath("(//span[text()='Sitemap'])[1]");
+    public static By clickOnkristallwelten=By.xpath("(//span[text()='Kristallwelten'])[1]");
+    public static By clickOnAboutUs=By.xpath("(//span[text()='About Us'])[1]");
+    public static By clickOnTermsOfUse=By.xpath("(//span[text()='Terms Of Use'])[1]");
+    public static By clickOnTermsAndConditions=By.xpath("(//span[text()='Terms & Conditions'])[1]");
+    public static By clickOnPrivacyPolicy=By.xpath("(//span[text()='Privacy Policy'])[1]");
+    public static By clickOn
 }
