3702218d70503a60c253cdee0c2a32559f40aad5
Safiur6296 <safiurrahaman6296@gmail.com>
2025-04-21 11:29:03 +0000

    scuffholding

--- Project/src/test/java/utils/LoggerHandler.java
+++ Project/src/test/java/utils/LoggerHandler.java
@@ -1,5 +1,51 @@
 package utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
 public class LoggerHandler {
 
+    private static final Logger logger = Logger.getLogger(LoggerHandler.class);
+
+    static {
+        try {
+            //creating the file with time stamp
+            String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+            String logFileName = "logs/logfile_" + timestamp + ".log";
+            
+            FileAppender fileAppender = new FileAppender(new PatternLayout("%d{ISO8601} %-5p - %m%n"), logFileName, true);
+            logger.addAppender(fileAppender);
+        } catch (Exception e) {
+            logger.error("Fail to genreate logs", e);
+        }
+    }
+    public static void trace(String message) {
+        logger.trace(message);
+    }
+
+    public static void debug(String message) {
+        logger.debug(message);
+    }
+
+    public static void info(String message) {
+        logger.info(message);
+    }
+
+    public static void warn(String message) {
+        logger.warn(message);
+    }
+
+    public static void error(String message) {
+        logger.error(message);
+    }
+
+    public static void fatal(String message) {
+        logger.fatal(message);
+    }
+
+    
 }
