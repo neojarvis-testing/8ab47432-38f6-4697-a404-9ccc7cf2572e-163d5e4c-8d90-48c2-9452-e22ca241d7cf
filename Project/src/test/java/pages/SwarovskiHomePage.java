36643f6f9a5ba6adc119f9dec6547c16d1f253a7
KARTIKBAMETA <kartikbameta09@gmail.com>
2025-04-21 16:32:52 +0000

    treeCleared

--- Project/src/test/java/pages/SwarovskiHomePage.java
+++ Project/src/test/java/pages/SwarovskiHomePage.java
@@ -1,8 +1,13 @@
 package pages;
 
-import com.aventstack.extentreports.ExtentTest;
+import org.testng.Assert;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import uistore.SwarovskiHomePageLocators;
 import utils.Base;
+import utils.LoggerHandler;
 import utils.WebDriverHelper;
 
 public class SwarovskiHomePage {
@@ -12,5 +17,113 @@ public class SwarovskiHomePage {
         helper = new WebDriverHelper(Base.driver);
         this.test=test;
     }
+    public void hoverOverGifts(){
+        helper.waitForElementToBeVisible(SwarovskiHomePageLocators.hoverOnGifts,10);
+        helper.hoverElement(SwarovskiHomePageLocators.hoverOnGifts);
+        test.log(Status.INFO,"Hover on Gifts");
+        LoggerHandler.logInfo("Hover on Gifts");
+    }
+    public void clickOnWeddingGifts(){
+        helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnWeddingGifts,10);
+        helper.clickOnElement(SwarovskiHomePageLocators.clickOnWeddingGifts);
+        test.log(Status.INFO, "Click on Wedding Gifts");
+        LoggerHandler.logInfo("Click on Wedding Gifts");
+    }
+    public void clickOnCtaegories(){
+        helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnCategories,10);
+        helper.clickOnElement(SwarovskiHomePageLocators.clickOnCategories);
+        test.log(Status.INFO, "Click on Categories");
+        LoggerHandler.logInfo("Click on Categories");
+    }
+    public void clickOnWatches(){
+        helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnWatches,10);
+        helper.clickOnElement(SwarovskiHomePageLocators.clickOnWatches);
+        test.log(Status.INFO, "Click on Watches");
+        LoggerHandler.logInfo("Click on Watches");
+    }
+    public void clickOnAllWatches(){
+        helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnAllWatches,10);
+        helper.clickOnElement(SwarovskiHomePageLocators.clickOnAllWatches);
+        test.log(Status.INFO,"Click on all watches");
+        LoggerHandler.logInfo("Click on all watches");
+    }
+    public void clickOnShowProducts(){
+        try {
+            helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnShowAll,10);
+            helper.clickOnElement(SwarovskiHomePageLocators.clickOnShowAll);
+            test.log(Status.INFO,"Click on Show Products");
+            LoggerHandler.logInfo("Click on Show Products");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void clickOnMaterial(){
+        try {
+            helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnMaterial,10);
+            helper.clickOnElement(SwarovskiHomePageLocators.clickOnMaterial);
+            test.log(Status.INFO,"Click on Material");
+            LoggerHandler.logInfo("Click on Material");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void clickOnStainlessSteel(){
+        try {
+            helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnStainlessSteel,10);
+            helper.clickOnElement(SwarovskiHomePageLocators.clickOnStainlessSteel);
+            test.log(Status.INFO,"Click on Material");
+            LoggerHandler.logInfo("Click on Material");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void clickOnShowAll(){
+        try {
+            helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnShowAllButton,10);
+            helper.clickOnElement(SwarovskiHomePageLocators.clickOnShowAllButton);
+            test.log(Status.INFO,"Click on Material");
+            LoggerHandler.logInfo("Click on Material");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void clickOnFirstProduct(){
+        try {
+            helper.waitForElementToBeVisible(SwarovskiHomePageLocators.clickOnFirstProduct,10);
+            helper.clickOnElement(SwarovskiHomePageLocators.clickOnFirstProduct);
+            test.log(Status.INFO,"Click on First Product");
+            LoggerHandler.logInfo("Click on First Product");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void verify(){
+        try {
+            String str = helper.getText(SwarovskiHomePageLocators.addToCartText);
+            Assert.assertEquals(str, "Add to bag");
+            test.log(Status.PASS, "Add To Bag Text Verified");
+            LoggerHandler.logInfo("Add to bag text verified");
+        } catch (AssertionError e) {
+            test.log(Status.FAIL, "Add To Bag Text Cannot be Verified");
+            LoggerHandler.logError("Add to bag text Cannot be verified");
+        }
+    }
+    
+    public void testSwarovskiGiftsPage(){
+        hoverOverGifts();
+        clickOnWeddingGifts();
+        clickOnCtaegories();
+        clickOnWatches();
+        clickOnAllWatches();
+        clickOnShowProducts();
+        clickOnMaterial();
+        clickOnStainlessSteel();
+        clickOnShowAll();
+        clickOnFirstProduct();
+        verify();
+    }
+
+
+    
 
 }
--- Project/src/test/java/uistore/SwarovskiHomePageLocators.java
+++ Project/src/test/java/uistore/SwarovskiHomePageLocators.java
@@ -7,11 +7,13 @@ public class SwarovskiHomePageLocators {
     public static By clickOnWeddingGifts = By.xpath("//a[@rel='Wedding gifts']/child::span[text()='Wedding gifts']");
     public static By clickOnCategories = By.xpath("//span[@class='swa-filter-category-desktop__filter-name swa-label-sans--small' and contains(text(),'Categories')]");
     public static By clickOnWatches = By.xpath("(//li[@class='swa-filter-list-item']/child::label[contains(text(),'Watches')])[2]");
-    public static By clickOnAllWatches = By.xpath("//label[contains(text(),'All watches')]");
+    public static By clickOnAllWatches = By.cssSelector("label[for='filter-category-0202']");
     public static By clickOnShowAll = By.cssSelector("button[class='swa-button swa-button--large swa-button--no-margin swa-button-primary-black swa-filter-button swa-filter-button--show swa-button--apply-all']>span[class='swa-button__content']>span[class='swa-button__text swa-label-sans--medium-strong']");
-    public static By clickOnFilter = By.id("filter-dropdown-count-product_material");
+    // public static By clickOnFilter = By.id("filter-dropdown-count-product_material");
+    public static By clickOnMaterial = By.id("filter-dropdown-count-product_material");
     public static By clickOnStainlessSteel = By.xpath("(//span[@class='swa-size-caption' and text()='Stainless steel'])[2]");
-    public static By clickOnButton = By.cssSelector("button[class='swa-button swa-button--large swa-button--no-margin swa-button-primary-black swa-filter-button swa-filter-button--show swa-button--apply-all']>span[class='swa-button__content']>span[class='swa-button__text swa-label-sans--medium-strong']");
+    public static By clickOnShowAllButton = By.cssSelector("button[class='swa-button swa-button--large swa-button--no-margin swa-button-primary-black swa-filter-button swa-filter-button--show swa-button--apply-all']>span[class='swa-button__content']>span[class='swa-button__text swa-label-sans--medium-strong']");
+    public static By clickOnFirstProduct = By.cssSelector("a[data-gtm-product-id='M5724515']>div[class='swa-product-tile-plp__information']>p>span[class='swa-product-tile-plp__information__title swa-product-sans--name']");
     public static By addToCartText = By.xpath("//span[text()='Add to bag']");
 
 
