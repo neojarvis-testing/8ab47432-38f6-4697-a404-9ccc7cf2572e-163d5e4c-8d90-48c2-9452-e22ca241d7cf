53338319a38cf3113ec34b0b83aa8036672227dc
Poojitha-2015 <poojitha.sudini@gmail.com>
2025-04-21 17:25:27 +0000

    ess

--- Project/src/test/java/runner/TestSwarovski.java
+++ Project/src/test/java/runner/TestSwarovski.java
@@ -1,19 +1,14 @@
 package runner;
-
-
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-
 import pages.SwarovskiHomePage;
 import utils.Base;
 import utils.Reporter;
-
 public class TestSwarovski extends Base {
     public static ExtentTest test;
     public  static ExtentReports report;
--- Project/src/test/java/uistore/SwarovskiHomePageLocators.java
+++ Project/src/test/java/uistore/SwarovskiHomePageLocators.java
@@ -4,9 +4,13 @@ import org.openqa.selenium.By;
 
 public class SwarovskiHomePageLocators {
     public static By hoverOnAccessories  = By.cssSelector("#navAccessoriesLink");
-    public static By clickOnSunglasses=By.cssSelector("#navAccessoriesSunglassesLink");
-    public static By clickOnMaterial=By.cssSelector("span[id='filter-dropdown-count-product_material']") ;
-    public static By clickOnAcetate=By.cssSelector("#dropdown-facet-value-list-product_material > li:nth-child(0)");
+    //public static By hoverOnAccessories=By.xpath("(//span[text()='Accessories'])[1]");
+    //public static By clickOnSunglasses=By.cssSelector("#navAccessoriesSunglassesLink");
+    public static By clickOnSunglasses=By.xpath("(//a[@id='navAccessoriesSunglassesLink'])[1]");
+    //public static By clickOnMaterial=By.cssSelector("span[id='filter-dropdown-count-product_material']") ;
+    public static By clickOnMaterial=By.xpath("//span[contains(text(),'Material')]");
+    //public static By clickOnAcetate=By.cssSelector("#dropdown-facet-value-list-product_material > li:nth-child(0)");
+    public static By clickOnAcetate=By.xpath("(//span[text()='Acetate'])[2]");
     public static  By clickOnShowProducts=By.cssSelector("span.swa-button__text swa-label-sans--medium-strong");
     public static By clickOnFirstProduct=By.xpath("//span[text()='Pillow shape, Purple']");
     public static By clickonCareandMaintainence=By.cssSelector("#tabby-toggle_swa-accessible-ALWAYSON_PDP_ACC-ENTRY_CARE_ROW");
