e8d4e343a4fc16b3229fdd8d90102a83029d82c9
nischith4008 <l.nischith20@gmail.com>
2025-04-21 13:25:59 +0000

    clean

--- Project/src/test/java/uistore/SwarovskiHomePageLocators.java
+++ Project/src/test/java/uistore/SwarovskiHomePageLocators.java
@@ -1,5 +1,17 @@
 package uistore;
 
-public class SwarovskiHomePageLocators {
+import org.openqa.selenium.By;
 
+public class SwarovskiHomePageLocators {
+public static By searchIcon = By.cssSelector("div[class='js-swa-search-layer-icon swa-header__sticky-search-icon'] a[gtm-event-uitext='Search'] span[class='swa-link__icon swa-link__icon--search js-swa-link__icon--search']");
+public static By searchBar = By.cssSelector("input[placeholder='Search']");
+public static By clickOnCustomerServiceOverview=By.xpath("(//span[text()='Customer Service Overview'])[1]");
+public static By clickOnOrderStatus=By.xpath("(//span[text()='Order Status'])[1]");
+public static By clickOnGiftCardBalance=By.xpath("(//span[text()='Gift Card Balance'])[1]");
+public static By clickOnShipping=By.xpath("(//span[text()='Shipping'])[1]");
+public static By clickOnReturnsAndExchange=By.xpath("(//span[text()='Returns & Exchange'])[1]");
+public static By clickOnContactUs=By.xpath("(//span[text()='Contact Us'])[1]");
+public static By clickOnSizeGuide=By.xpath("(//span[text()='Size Guide'])[1]");
+public static By clickOnStoreFinder=By.xpath("(//span[text()='Store Finder'])[1]");
+public static By clickOnMembership=By.xpath("(//span[text()='Membership'])[1]");
 }
